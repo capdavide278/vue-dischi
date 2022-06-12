f794050ba4236a336e5f17038c2281ca0944b694
Davide <franc.davi00@gmail.com>
2022-06-12 16:44:44 +0200

    inserimento props e immagini

---
+++ src/components/DiscCard.vue
@@ -0,0 +1,56 @@
+<template>
+    <section>
+        <div id="disco">
+            <img :src="discDescription.poster" :alt="discDescription.title">
+            <div id="info">
+                <h2>{{discDescription.title}}</h2>
+                <h4>{{discDescription.author}}</h4>
+                <h4>{{discDescription.genre}}</h4>
+                <h4>{{discDescription.year}}</h4>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'DiscCard',
+  props: {
+    discDescription : Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#disco{
+   /*  flex-basis: calc(100% / 5); */
+    height: 80%;
+    background-color: #2e3a46;
+    margin: 50px;
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+img{
+    width: 170px;
+    /* height: 200px; */
+    padding: 20px 0;
+}
+
+h2{
+    color:white;
+    text-align: center;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+h4{
+    color: #495a6e;
+    text-align: center;
+}
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,32 +1,40 @@
 <template>
     <main>
         <div id="background_copertine">
-
+          <DiscCard
+            v-for="(item, index) in album" :key="index"
+            :discDescription="item"
+          />
         </div>
     </main>
 </template>
 
 <script>
 import axios from "axios"
+import DiscCard from "./DiscCard.vue";
+
 
 export default {
-  name: 'myMain',
-
-  data(){
-    return{
-      apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
-      copertine_album : []
+    name: "myMain",
+    data() {
+        return {
+            apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+            album: []
+        };
+    },
+    created() {
+        axios.get(this.apiUrl)
+            .then(result => {
+            this.album = result.data.response;
+            console.log(result);
+        })
+            .catch(error => {
+            console.log("Errore", error);
+        });
+    },
+    components: { 
+      DiscCard 
     }
-  },
-  craeted(){
-    axios.get(this.apiUrl)
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => {
-      console.log("Errore", error);
-    })
-  }
 }
 </script>
 
@@ -34,7 +42,14 @@ export default {
 <style scoped lang="scss">
 main{
     background-color: #1e2d3b;
-    height: 100vh;
+    /* height: 100vh; */
+}
+
+#background_copertine{
+  display: flex;
+  flex-wrap: wrap;
+  width: 80%;
+  margin: auto;
 }
 
 </style>
